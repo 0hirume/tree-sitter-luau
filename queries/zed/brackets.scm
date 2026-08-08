bac3b936df57e4447ebe2115a178dc6c26d59940
ohirume <0uhirume@gmail.com>
2026-08-08 10:51:29 +0800

    feat(queries): add Zed query subset

---
+++ queries/zed/brackets.scm
@@ -0,0 +1,8 @@
+"(" @open ")" @close
+"[" @open "]" @close
+"{" @open "}" @close
+
+(explicit_type_arguments "<<" @open ">>" @close)
+(generic_parameter_list "<" @open ">" @close)
+(generic_definition_list "<" @open ">" @close)
+(type_reference "<" @open ">" @close)
---
+++ queries/zed/outline.scm
@@ -0,0 +1,24 @@
+(function_declaration
+  name: (function_name name: (identifier) @name)) @item
+(function_declaration
+  name: (function_name field: (identifier) @name)) @item
+(function_declaration
+  name: (function_name method: (identifier) @name)) @item
+
+(local_function_declaration name: (identifier) @name) @item
+(const_function_declaration name: (identifier) @name) @item
+(type_function_declaration name: (identifier) @name) @item
+(class_declaration name: (identifier) @name) @item
+(class_method name: (identifier) @name) @item
+(type_alias_declaration name: (identifier) @name) @item
+
+(assignment_statement
+  left: (assignment_target_list
+    (identifier) @name)
+  right: (expression_list
+    (function_expression))) @item
+(assignment_statement
+  left: (assignment_target_list
+    (field_expression field: (identifier) @name))
+  right: (expression_list
+    (function_expression))) @item
